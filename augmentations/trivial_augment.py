1ebcd8a5c71b46459eefd70b50f0f33ff01bb004
Ekagra Gupta <ekagrag99@gmail.com>
2024-11-18 18:54:00 +0100

    shearx

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -145,11 +145,11 @@ class CustomTrivialAugmentWide(torch.nn.Module):
     def _augmentation_space(self, num_bins: int) -> Dict[str, Tuple[Tensor, bool]]:
         return {
             "Identity": (torch.tensor(0.0), False),
-            # "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
+            "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
             # "ShearY": (torch.linspace(0.0, 0.99, num_bins), True),
             # "TranslateX": (torch.linspace(0.0, 32.0, num_bins), True),
             # "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
-            "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
+            # "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
             # "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Color": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
@@ -545,6 +545,8 @@ class CustomTrivialAugmentWide(torch.nn.Module):
         augmentation_severity = abs(
             int(augmentation_magnitude / max_magnitude * self.num_magnitude_bins)
         )
+
+        print(f'Severity: {augmentation_severity}\tAugmentation info: {augment_info}\n')
         if augmentation_type == "Solarize":
             augmentation_severity = self.num_magnitude_bins - augmentation_severity
 
