abaf42b503d4bd9148b8b206a245b644aca6bd1b
Ekagra Gupta <ekagrag99@gmail.com>
2024-11-19 11:51:02 +0100

    Posterize

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -153,11 +153,11 @@ class CustomTrivialAugmentWide(torch.nn.Module):
             # "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Color": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
-            "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Posterize": (
-            #     8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(),
-            #     False,
-            # ),
+            # "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Posterize": (
+                8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(),
+                False,
+            ),
             # "Solarize": (torch.linspace(255.0, 0.0, num_bins), False),
             # "AutoContrast": (torch.tensor(0.0), False),
             # "Equalize": (torch.tensor(0.0), False),
