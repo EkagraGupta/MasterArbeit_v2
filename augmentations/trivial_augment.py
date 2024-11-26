7fb26d54b94e8429d786b4b6458b978712e6f7ac
Ekagra Gupta <ekagrag99@gmail.com>
2024-11-26 15:33:31 +0100

    contrast

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -150,9 +150,9 @@ class CustomTrivialAugmentWide(torch.nn.Module):
             # "TranslateX": (torch.linspace(0.0, 32.0, num_bins), True),
             # "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
             # "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
-            "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
+            # "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Color": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
             # "Posterize": (
             #     8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(),
