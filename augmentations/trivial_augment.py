2584f746134801c07e4480a4e64fa54d78b63ded
Ekagra Gupta <ekagrag99@gmail.com>
2024-11-30 10:52:49 +0100

    update

--- augmentations/trivial_augment.py
+++ augmentations/trivial_augment.py
@@ -145,22 +145,22 @@ class CustomTrivialAugmentWide(torch.nn.Module):
     def _augmentation_space(self, num_bins: int) -> Dict[str, Tuple[Tensor, bool]]:
         return {
             "Identity": (torch.tensor(0.0), False),
-            # "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "ShearY": (torch.linspace(0.0, 0.99, num_bins), True),
+            "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
+            "ShearY": (torch.linspace(0.0, 0.99, num_bins), True),
             "TranslateX": (torch.linspace(0.0, 32.0, num_bins), True),
-            # "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
-            # "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
-            # "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Color": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
-            # "Posterize": (
-            #     8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(),
-            #     False,
-            # ),
-            # "Solarize": (torch.linspace(255.0, 0.0, num_bins), False),
-            # "AutoContrast": (torch.tensor(0.0), False),
-            # "Equalize": (torch.tensor(0.0), False),
+            "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
+            "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
+            "Brightness": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Color": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Contrast": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Sharpness": (torch.linspace(0.0, 0.99, num_bins), True),
+            "Posterize": (
+                8 - (torch.arange(num_bins) / ((num_bins - 1) / 6)).round().int(),
+                False,
+            ),
+            "Solarize": (torch.linspace(255.0, 0.0, num_bins), False),
+            "AutoContrast": (torch.tensor(0.0), False),
+            "Equalize": (torch.tensor(0.0), False),
         }
 
         # print(f'augmentation_name: {self.augmentation_name}\tseverity: {self.severity}')
